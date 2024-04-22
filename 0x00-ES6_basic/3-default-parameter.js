ff0197cdd51732e70b2fd13ba404014337eb0311
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-04-22 13:22:55 +0300

    Parameter defalts

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,4 @@
+export default function getSumOfHoods(initialNumber, expansion1989=89, expansion2019=19) {
+  if (expansion1989 === undefined) {
+  return initialNumber + expansion1989 + expansion2019;
+}
